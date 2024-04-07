e2f397a5ab30031647258e037aba9161b1d5645f
“1771559253” <1771559253@qq.com>
2024-04-07 15:27:49 +0800

    删除套餐业务代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,7 +11,7 @@ public interface SetmealDishMapper {
 
     /**
      * 根据菜品id查询对应的套餐id
-     * @param ids
+     * @param dishIds
      * @return
      */
     List<Long> getSetMealIdsByDishIds(List<Long> dishIds);
@@ -20,4 +21,12 @@ public interface SetmealDishMapper {
      * @param setmealDishes
      */
     void insertBatch(List<SetmealDish> setmealDishes);
+
+
+    /**
+     * 根据套餐id删除套餐和菜品的关联关系
+     * @param setmealId
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long setmealId);
 }
